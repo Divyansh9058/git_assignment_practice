3d278343dbda1d27837fbfa79ffb166c90329c06
Divyansh <divyanshgoel777@gmail.com>
2022-10-19 23:11:37 +0530

    first commit prime and palindrome add

---
+++ Palindrome.js
@@ -0,0 +1,11 @@
+let str="naman";
+
+let bag="";
+
+for(let i=str.length;i>=0;i--){
+bag+=str[i];
+}
+
+if (str==bag){
+    console.bag(str,"is palindrome");
+}
---
+++ Prime.js
@@ -0,0 +1,11 @@
+let num=13 ;
+let count=0;
+for(let i=1;i<=num;i++){
+if(num%i==0){
+count++
+}
+}
+
+if(count==0){
+console.log(num,"is prime")
+}
